5e3012bb1d0762aaa0ba73c7651b99389695e224
chenweizi <chenweizi.chen@bytedance.com>
2023-02-22 13:01:45 +0800

    feat: 修改 buffer 的插件以及 leap 的修改

--- lua/config/enhanceKeymaps.lua
+++ lua/config/enhanceKeymaps.lua
@@ -3,6 +3,8 @@ local vimcmd = vim.cmd;
 local utils = require("utils")
 
 
+-- 方便常用的快捷键
+
 -- 非常重要的 keymap, 关于屏幕分屏的
 map("n", "s", "")
 map("n", "ss", ":split<Return>", { desc = "横向分屏" })
@@ -18,6 +20,16 @@ map("n", "so", "<C-w>o", { desc = "横向分屏" })
 map("n", "sn", "<C-w>=", { desc = "横向分屏" })
 map("n", "sp", "<C-w>=", { desc = "横向分屏" })
 
+map("n", "<leader>so", "<cmd>BufferCloseAllButCurrentOrPinned<CR>", { desc = "close other but pinned" })
+map("n", "<leader>sO", "<cmd>BufferCloseAllButCurrent<CR>", { desc = "close other but current" })
+map("n", "<leader>sp", "<cmd>BufferPin<CR>", { desc = "pin tab" })
+
 
 -- 非常重要的 tab 动作
 map("n", "tc", ":tabclose<CR>", { desc = "close tab" })
+map("n", "to", ":tabonly<CR>", { desc = "close other tab" })
+map("n", "tn", function()
+  local cursor = vim.fn.line('.')
+  vimcmd('tabnew %')
+  vimcmd(tostring(cursor))
+end, { desc = "new tab" })
--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -3,15 +3,28 @@ return {
   {
     "ggandor/leap.nvim",
     event = "VeryLazy",
-    dependencies = { { "ggandor/flit.nvim", opts = { labeled_modes = "nv" } } },
     config = function(_, opts)
-      -- local leap = require("leap")
+      local leap = require("leap")
       -- for k, v in pairs(opts) do
       --   print("k is", k)
       --   print("v is", v)
       --   leap.opts[k] = v
       -- end
-      -- leap.add_default_mappings(true)
+      leap.add_default_mappings(false)
     end,
+    opts = {
+      safe_labels = {
+        "s", "f", "n",
+        "j", "k", "l", "h", "o", "d", "w", "e", "m", "b",
+        "u", "y", "v", "r", "g", "t", "c", "x", "/", "z",
+        "S", "F", "N",
+        "J", "K", "L", "H", "O", "D", "W", "E", "M", "B",
+        "U", "Y", "V", "R", "G", "T", "C", "X", "?", "Z"
+      },
+    },
+    keys = {
+      { ",s", "<Plug>(leap-forward-to)" },
+      { ",S", "<Plug>(leap-backward-to)" },
+    }
   },
 }
