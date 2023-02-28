c605a3ef6299362009f01be0bb3bec3d1f3279ff
chenweizi <chenweizi.chen@bytedance.com>
2023-02-28 12:48:53 +0800

    feat: add harpoon

--- lua/plugins/enhancePlugin.lua
+++ lua/plugins/enhancePlugin.lua
@@ -1,5 +1,41 @@
 return {
   {
+    "ThePrimeagen/harpoon",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    config = function()
+      require("telescope").load_extension("harpoon")
+    end,
+    keys = {
+      {
+        ";a",
+        function()
+          print("add marks")
+          require("harpoon.mark").add_file()
+        end,
+        "add harpoon mark",
+      },
+      {
+        ";h",
+        ":Telescope harpoon marks<cr>",
+        "find harpoon marks",
+      },
+      {
+        "<leader>ha",
+        function()
+          print("add marks")
+          require("harpoon.mark").add_file()
+        end,
+        "add harpoon mark",
+      },
+      {
+        "<leader>hs",
+        ":Telescope harpoon marks<cr>",
+        "find harpoon marks",
+      },
+    },
+  },
+
+  {
     "windwp/nvim-ts-autotag",
   },
 
