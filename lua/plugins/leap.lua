bb2bfdbc47aab3f148b21778cb8f6d24d0d78ce1
chenweizi <chenweizi.chen@bytedance.com>
2023-03-21 11:44:04 +0800

    fix: leap nvim

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -5,15 +5,24 @@ return {
     event = "VeryLazy",
     dependencies = { { "ggandor/flit.nvim", opts = { labeled_modes = "nv" }, enabled = true } },
     config = function(_, opts)
+      -- 全屏幕的跳转
+      vim.keymap.set("n", ";s", function()
+        local focusable_windows_on_tabpage = vim.tbl_filter(function(win)
+          return vim.api.nvim_win_get_config(win).focusable
+        end, vim.api.nvim_tabpage_list_wins(0))
+        require("leap").leap({ target_windows = focusable_windows_on_tabpage })
+      end, { desc = "jump all windows" })
+
       local leap = require("leap")
       for k, v in pairs(opts) do
         leap.opts[k] = v
       end
-      -- leap.add_default_mappings(true)
+      leap.add_default_mappings(true)
     end,
     keys = {
-      { ",s", "<Plug>(leap-forward-to)" },
-      { ",S", "<Plug>(leap-backward-to)" },
+      -- { ",s", "<Plug>(leap-forward-to)" },
+      -- { ",s", "<Plug>(leap-from-window)" },
+      -- { ",S", "<Plug>(leap-backward-to)" },
     },
   },
 }
