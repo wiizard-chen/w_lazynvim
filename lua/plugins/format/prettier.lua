f64f07cbf13072dfa44a3bf99396a682ad592315
chenweizi <chenweizi.chen@bytedance.com>
2023-04-03 23:54:31 +0800

    feat: test

--- lua/plugins/format/prettier.lua
+++ lua/plugins/format/prettier.lua
@@ -7,15 +7,16 @@ return {
   {
     "williamboman/mason.nvim",
     opts = function(_, opts)
-      print("masson ")
+      print("masson my masson fucking ")
       table.insert(opts.ensure_installed, "prettierd")
     end,
   },
   {
     "jose-elias-alvarez/null-ls.nvim",
+    dependencies = { "mason.nvim" },
     opts = function(_, opts)
       local nls = require("null-ls")
-      print("null-ls ")
+      print("null-ls prettierd ")
       table.insert(opts.sources, nls.builtins.formatting.prettierd)
     end,
   },
