bdbdce68e6634f2632ac103a4aab8ce4fba2cfd4
chenweizi <chenweizi.chen@bytedance.com>
2023-04-08 14:11:17 +0800

    fix: improve telescope

--- lua/plugins/improve/harpoon.lua
+++ lua/plugins/improve/harpoon.lua
@@ -4,7 +4,7 @@ return {
     dependencies = { "nvim-telescope/telescope.nvim" },
     config = function()
       require("telescope").load_extension("harpoon")
-      local map = require("utils").map
+      local map = require("utils.init").map
       map("n", ";a", function()
         print("add marks")
         require("harpoon.mark").add_file()
