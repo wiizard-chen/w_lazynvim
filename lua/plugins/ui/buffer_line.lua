a4dbab39489d2531c1a0ae3826708d49282b3c01
weizi.chen <weizi.chen@weizichens-MacBook-Pro.local>
2023-10-16 08:53:58 +0800

    fix: barbar error

--- lua/plugins/ui/buffer_line.lua
+++ lua/plugins/ui/buffer_line.lua
@@ -7,7 +7,7 @@ return {
     "romgrk/barbar.nvim",
     dependencies = { { "nvim-tree/nvim-web-devicons" } },
     enabled = true,
-    event = "VeryLazy",
+    -- event = "VeryLazy",
     opts = {
       -- Enables/disable clickable tabs
       --  - left-click: go to buffer
@@ -25,39 +25,39 @@ return {
         ["neo-tree"] = { event = "BufWipeout" },
       },
 
-      icons = {
-        -- Configure the base icons on the bufferline.
-        buffer_index = false,
-        buffer_number = false,
-        button = "󰅙",
-        -- Enables / disables diagnostic symbols
-        diagnostics = {
-          [vim.diagnostic.severity.ERROR] = { enabled = false, icons = "" },
-          [vim.diagnostic.severity.WARN] = { enabled = false },
-          [vim.diagnostic.severity.INFO] = { enabled = false },
-          [vim.diagnostic.severity.HINT] = { enabled = false },
-        },
-        filetype = {
-          -- Sets the icon's highlight group.
-          -- If false, will use nvim-web-devicons colors
-          custom_colors = false,
-
-          -- Requires `nvim-web-devicons` if `true`
-          enabled = true,
-        },
-
-        -- Configure the icons on the bufferline when modified or pinned.
-        -- Supports all the base icon options.
-        modified = { button = "●" },
-        pinned = { button = "󰐃", filename = true, separator = { right = "" } },
-      },
+      -- icons = {
+      --   -- Configure the base icons on the bufferline.
+      --   buffer_index = false,
+      --   buffer_number = false,
+      --   button = "󰅙",
+      --   -- Enables / disables diagnostic symbols
+      --   diagnostics = {
+      --     [vim.diagnostic.severity.ERROR] = { enabled = false, icons = "" },
+      --     [vim.diagnostic.severity.WARN] = { enabled = false },
+      --     [vim.diagnostic.severity.INFO] = { enabled = false },
+      --     [vim.diagnostic.severity.HINT] = { enabled = false },
+      --   },
+      --   filetype = {
+      --     -- Sets the icon's highlight group.
+      --     -- If false, will use nvim-web-devicons colors
+      --     custom_colors = false,
+      --
+      --     -- Requires `nvim-web-devicons` if `true`
+      --     enabled = true,
+      --   },
+      --
+      --   -- Configure the icons on the bufferline when modified or pinned.
+      --   -- Supports all the base icon options.
+      --   modified = { button = "●" },
+      --   pinned = { button = "󰐃", filename = true, separator = { right = "" } },
+      -- },
     },
-    config = function(_, opts)
-      require("barbar").setup(opts)
-      -- local hl = require("bufferline.utils").hl
-      -- local fg_current = hl.fg_or_default({ "Normal" }, "#ff00ff", 255)
-      -- local bg_current = hl.bg_or_default({ "Normal" }, "none")
-      -- hl.set("BufferDefaultCurrent", bg_current, fg_current)
-    end,
+    -- config = function(_, opts)
+    --   require("barbar").setup(opts)
+    --   -- local hl = require("bufferline.utils").hl
+    --   -- local fg_current = hl.fg_or_default({ "Normal" }, "#ff00ff", 255)
+    --   -- local bg_current = hl.bg_or_default({ "Normal" }, "none")
+    --   -- hl.set("BufferDefaultCurrent", bg_current, fg_current)
+    -- end,
   },
 }
