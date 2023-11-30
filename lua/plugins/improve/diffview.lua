a8bf5c9ce61b411e7c2a17cd0dba684a7afb8d9a
chenweizi <weizi.chen@kone.com>
2023-11-30 15:28:26 +0800

    fix: diffview keymap error

--- lua/plugins/improve/diffview.lua
+++ lua/plugins/improve/diffview.lua
@@ -1,7 +1,6 @@
 return {
   {
     "sindrets/diffview.nvim",
-    -- enbale = false,
     dependencies = { { "nvim-lua/plenary.nvim" }, { "nvim-web-devicons" } },
     opts = {
       keymaps = {
@@ -17,8 +16,8 @@ return {
     },
     keys = {
       { "<leader>gl", ":DiffviewFileHistory %<CR>", desc = "open current file history" },
-      { "<leader>go", ":DiffviewOpen<CR>", desc = "open current file history" },
-      -- { "<leader>gc", ":DiffviewClose<CR>", desc = "close diff view" },
+      { "<leader>go", ":DiffviewOpen<CR>", desc = "open current project" },
+      { "<leader>gd", ":tabclose<CR>", desc = "close current project" },
     },
   },
 }
