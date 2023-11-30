686e5f6db90952d5d6810e8a363b858a4b37765a
chenweizi <weizi.chen@kone.com>
2023-11-30 15:15:09 +0800

    WIP

--- lua/plugins/improve/diffview.lua
+++ lua/plugins/improve/diffview.lua
@@ -3,6 +3,18 @@ return {
     "sindrets/diffview.nvim",
     -- enbale = false,
     dependencies = { { "nvim-lua/plenary.nvim" }, { "nvim-web-devicons" } },
+    opts = {
+      keymaps = {
+        file_panel = {
+          {
+            "n",
+            "s",
+            false,
+            { desc = "Stage / unstage the selected entry" },
+          },
+        },
+      },
+    },
     keys = {
       { "<leader>gl", ":DiffviewFileHistory %<CR>", desc = "open current file history" },
       { "<leader>go", ":DiffviewOpen<CR>", desc = "open current file history" },
