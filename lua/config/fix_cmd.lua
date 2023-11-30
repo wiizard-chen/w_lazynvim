757fb482b3759cd3feecc49213ed44c2e81e9531
chenweizi <weizi.chen@kone.com>
2023-11-30 11:49:21 +0800

    fix: try to add focus

--- lua/config/fix_cmd.lua
+++ lua/config/fix_cmd.lua
@@ -18,3 +18,32 @@ augroup KeyMapAutoCommands
   autocmd BufEnter * lua local map = require("utils.init").map; map("i", "<C-k>", '<space><Esc>"_d$a', { desc = "delete the forward of line" })
 augroup END
 ]])
+
+-- local ignore_filetypes = { "neo-tree" }
+-- local ignore_buftypes = { "nofile", "prompt", "popup" }
+--
+-- local augroup = vim.api.nvim_create_augroup("FocusDisable", { clear = true })
+--
+-- vim.api.nvim_create_autocmd("WinEnter", {
+--   group = augroup,
+--   callback = function(_)
+--     if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
+--       vim.w.focus_disable = true
+--     else
+--       vim.w.focus_disable = false
+--     end
+--   end,
+--   desc = "Disable focus autoresize for BufType",
+-- })
+--
+-- vim.api.nvim_create_autocmd("FileType", {
+--   group = augroup,
+--   callback = function(_)
+--     if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+--       vim.b.focus_disable = true
+--     else
+--       vim.b.focus_disable = false
+--     end
+--   end,
+--   desc = "Disable focus autoresize for FileType",
+-- })
