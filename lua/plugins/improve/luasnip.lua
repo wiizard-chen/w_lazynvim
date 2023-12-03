b76c6ed49c1624f15ea8aa4c1a72c178ba7a9eac
wiizard-chen <wiizard-chen@outlook.com>
2023-12-03 16:09:42 +0800

    fix: luasnip version lock

--- lua/plugins/improve/luasnip.lua
+++ lua/plugins/improve/luasnip.lua
@@ -2,6 +2,10 @@ return {
   -- snippets
   {
     "L3MON4D3/LuaSnip",
+    commit = "1def35377854535bb3b0f4cc7a33c083cdb12571",
+    build = (not jit.os:find("Windows"))
+        and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
+      or nil,
     dependencies = {
       "rafamadriz/friendly-snippets",
       config = function()
