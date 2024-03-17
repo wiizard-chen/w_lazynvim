44c3ba5f39164cbfb49b4a35941abe1b6729ac52
wiizard-chen <wiizard-chen@outlook.com>
2024-03-17 14:27:35 +0800

    WIP

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -31,6 +31,7 @@ require("lazy").setup({
     -- { import = "plugins.formatting.prettier" },
     -- { import = "plugins.formatting.codeium" },
     { import = "plugins.extralang.flutter" },
+    { import = "plugins.extralang.python" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
