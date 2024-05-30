1fe7ae0b18dc0c9378c8cd1a62a691d0ed975205
weizi.chen <weizi.chen@kone.com>
2024-05-30 11:24:30 +0800

    WIP

--- lua/plugins/improve/luasnip.lua
+++ lua/plugins/improve/luasnip.lua
@@ -1,23 +1,37 @@
 return {
-  -- snippets
-  {
-    "L3MON4D3/LuaSnip",
-    commit = "1def35377854535bb3b0f4cc7a33c083cdb12571",
-    build = (not jit.os:find("Windows"))
-        and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
-      or nil,
-    dependencies = {
+  "L3MON4D3/LuaSnip",
+  build = (not LazyVim.is_win())
+      and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
+    or nil,
+  dependencies = {
+    {
       "rafamadriz/friendly-snippets",
       config = function()
         require("luasnip.loaders.from_vscode").lazy_load({
-          paths = vim.fn.stdpath("config") .. "/mysnippets",
+          -- paths = vim.fn.stdpath("config") .. "/mysnippets",
+          paths = "~/.config/nvim/mysnippets",
         })
       end,
     },
-    opts = {
-      history = true,
-      delete_check_events = "TextChanged",
+    {
+      "nvim-cmp",
+      dependencies = {
+        "saadparwaiz1/cmp_luasnip",
+      },
+      opts = function(_, opts)
+        opts.snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        }
+        table.insert(opts.sources, { name = "luasnip" })
+      end,
     },
+  },
+  opts = {
+    history = true,
+    delete_check_events = "TextChanged",
+  },
     -- stylua: ignore
     keys = {
       {
@@ -30,5 +44,4 @@ return {
       { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
       { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
     },
-  },
 }
--- lua/plugins/ui/neo_tree.lua
+++ lua/plugins/ui/neo_tree.lua
@@ -107,7 +107,7 @@ end
 return {
   {
     "rcarriga/nvim-notify",
-    -- enabled = false,
+    enabled = false,
     opts = {
       background_colour = "#ffffff",
     },
